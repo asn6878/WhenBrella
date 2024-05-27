911cb180b4fca7c8626cfe57b1a7dc5dceeb0984
DinoDeveloper <asn6878@gmail.com>
2024-05-27 20:40:39 +0900

    initial commit

---
+++ src/main/java/com/example/whenbrella/WhenbrellaApplication.java
@@ -0,0 +1,13 @@
+package com.example.whenbrella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhenbrellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhenbrellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whenbrella/WhenbrellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whenbrella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhenbrellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
