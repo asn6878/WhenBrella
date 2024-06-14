a6b109ae084cc72b7c6b3b1db152bd0767cd125c
asn6878 <asn6878@gmail.com>
2024-06-14 22:06:15 +0900

    feat: 전역 예외 핸들러 작성

---
+++ src/main/java/com/example/whenbrella/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.whenbrella.global.exception;
+
+
+import com.example.whenbrella.global.response.ApiExceptionResponse;
+import com.example.whenbrella.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    protected ApiResponse<ApiExceptionResponse> handleException(Exception ex) {
+        log.error("예외 발생: " + ex.toString());
+
+        final ApiExceptionResponse apiExceptionResponse = new ApiExceptionResponse(
+                500,
+                ex.toString()
+        );
+        return ApiResponse.createError(apiExceptionResponse);
+    }
+
+
+}
