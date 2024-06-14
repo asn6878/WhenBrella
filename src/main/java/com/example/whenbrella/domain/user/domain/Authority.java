6ba704b34b0b523937176728509379459e521c00
asn6878 <asn6878@gmail.com>
2024-06-14 21:28:27 +0900

    feat: User Entity 작성

---
+++ src/main/java/com/example/whenbrella/domain/user/domain/Authority.java
@@ -0,0 +1,6 @@
+package com.example.whenbrella.domain.user.domain;
+
+public enum Authority {
+    ROLE_USER, ROLE_ADMIN
+}
+
---
+++ src/main/java/com/example/whenbrella/domain/user/domain/User.java
@@ -0,0 +1,36 @@
+package com.example.whenbrella.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String phone;
+    private boolean isNotification;
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+
+    @Builder
+    public User(Long id, String username, String password, String phone, boolean isNotification) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.isNotification = isNotification;
+        this.authority = Authority.ROLE_USER;
+    }
+
+    public void updateNotification() {
+        this.isNotification = !this.isNotification;
+    }
+}
