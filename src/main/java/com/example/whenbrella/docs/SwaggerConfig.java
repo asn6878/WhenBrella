7dc5d581ad306a13473b568a8dafc1db56405517
asn6878 <asn6878@gmail.com>
2024-06-14 22:06:42 +0900

    feat: JWT 인증 구현

---
+++ src/main/java/com/example/whenbrella/docs/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.example.whenbrella.docs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "JWT API 명세",
+                description = "TestApp API 명세",
+                version = "v1"),
+        servers = {
+                @Server(url = "127.0.0.1:8080", description = "Local"),
+        }
+)
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi OpenApi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("TestApp API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
---
+++ src/main/java/com/example/whenbrella/security/SecurityConfig.java
@@ -0,0 +1,103 @@
+package com.example.whenbrella.security;
+
+import com.example.whenbrella.global.security.handler.CustomAuthenticationEntryPoint;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableMethodSecurity
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class SecurityConfig {
+
+    private static final String[] publicEndpoints = {
+            "/api/v1/auth/signin",
+            "/api/v1/auth/signup",
+
+            /* swagger v3*/
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+    };
+
+    private List<String> corsOrigins = List.of("http://localhost:3000", "http://localhost:8080");
+
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final JwtSecurityConfig jwtSecurityConfig;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
+                .sessionManagement(sess -> sess.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(getPublicEndpoints()).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(auth -> auth
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                );
+        jwtSecurityConfig.configure(httpSecurity);
+
+        return httpSecurity.build();
+    }
+
+/*    @Bean
+    CorsConfigurationSource corsConfigurationSource() { // Localhost 환경 cors
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(corsOrigins);
+        configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }*/
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*")); // 모든 출처 허용
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 모든 HTTP 메서드 허용
+        configuration.setAllowedHeaders(Collections.singletonList("*")); // 모든 헤더 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    /*AntPathRequestMatcher를 사용하면 추후 메소드 설정 등 더 유연한 경로를 매칭할 수 있다.*/
+    private RequestMatcher[] getPublicEndpoints() {
+        return Stream.concat(
+                Stream.of(publicEndpoints).map(AntPathRequestMatcher::new),
+                Stream.of(new AntPathRequestMatcher("/api/v1/post", "GET")) // GET 요청에 대해서만 permitAll
+        ).toArray(RequestMatcher[]::new);
+    }
+}
